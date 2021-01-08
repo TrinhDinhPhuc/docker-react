93358a46d9713160effa5b751ad020e63a66147b
phuccoi996@gmail.com <phuccoi996@gmail.com>
2021-01-08 17:49:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Motherfucking Flaming are in the house.
+        Yooooo
         </p>
         <a
           className="App-link"
